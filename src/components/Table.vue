f12b1f4f888fe32b62373f5cf6bee55f8b4c0c96
zhoucheng <zhoucheng133@outlook.com>
2025-08-26 19:00:21 +0800

    改进表格显示

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -8,22 +8,42 @@
 </template>
 
 <script lang="ts" setup>
+import { h } from 'vue';
 import data from '../utils/data';
+import type { DataItem } from '../utils/data_interface';
 
 const height=window.innerHeight/2-10
 
 const columns: any=[
   {
     title: "Time",
-    key: "timestamp"
+    key: "timestamp",
+    render(row: DataItem){
+      return h(
+        "div",
+        row.timestamp.slice(11, -3)
+      )
+    }
   },
   {
     title: "Temperature",
-    key: "temperature"
+    key: "temperature",
+    render(row: DataItem){
+      return h(
+        "div",
+        row.temperature+" ℃"
+      )
+    }
   },
   {
     title: "Humidity",
-    key: "humidity"
+    key: "humidity",
+    render(row: DataItem){
+      return h(
+        "div",
+        row.humidity+" %"
+      )
+    }
   }
 ];
 
